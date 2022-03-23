7dcec42b033e57e6bd78403eadcc1f1546b8b0eb
DINESH SK <dineshsrinivasan2001@gmail.com>
2022-03-23 10:26:29 +0530

    Delete TestYantra-CoreJava directory

--- TestYantra-CoreJava/Basics/Audi.java
+++
@@ -1,8 +0,0 @@
-package com.tyss.corejava;
-
-public class Audi extends Car{
-  @Override
-  public void brake() {
-	  System.out.println("New Brake Installed");
-  }
-}
--- TestYantra-CoreJava/Basics/Ferrari.java
+++
@@ -1,9 +0,0 @@
-package com.tyss.corejava;
-
-public class Ferrari extends Car{
-	@Override
-	public void drive() {
-		System.out.println("this is a v8 cylinder engine");
-	}
-
-}
